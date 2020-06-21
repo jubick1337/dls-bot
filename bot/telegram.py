e24d3b4576b588befa86e1fb6b2cd191c9536fad
jubick1337 <mattyson.so@gmail.com>
2020-06-21 19:00:18 +0500

    removed asyncgram

--- bot/telegram.py
+++ bot/telegram.py
@@ -1,99 +1,72 @@
-import logging
-
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import Text
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.utils import executor
-
-from model.nst import NST
-
 TELEGRAM_TOKEN = '1109017372:AAG7i7w4mDWlY9oUqaDQW8rN2NM105OODZ4'
 
-bot = Bot(token=TELEGRAM_TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
+import logging
+import ssl
+
+import telebot
+from aiohttp import web
+
+WEBHOOK_HOST = '35.204.74.60'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+WEBHOOK_SSL_CERT = './url_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './url_private.key'  # Path to the ssl private key
+
+WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(TELEGRAM_TOKEN)
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
+app = web.Application()
 
 
-# States
-class Form(StatesGroup):
-    command = State()
-    content = State()
-    style = State()
-
-
-@dp.message_handler(commands='start')
-async def handle_start(message: types.Message):
-    await Form.command.set()
-    return await message.reply("Hi there! Use one of the commands: /nst")
-
-
-@dp.message_handler(state='*', commands='cancel')
-@dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
-async def cancel_handler(message: types.Message, state: FSMContext):
-    """
-    Allow user to cancel any action
-    """
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-
-    logging.info('Cancelling state %r', current_state)
-    # Cancel state and inform user about it
-    await state.finish()
-    # And remove keyboard (just in case)
-    return await message.reply('Cancelled.')
-
-
-@dp.message_handler(state=Form.command)
-async def process_command(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['command'] = message.text
-
-    await Form.next()
-    return await message.reply("Now send content photo")
-
-
-@dp.message_handler(content_types=['photo', 'document'], state=Form.content)
-async def process_content(message: types.Message):
-    await Form.next()
-    if message.photo:
-        await message.photo[-1].download(f'content{message.chat.id}.jpg')
+# Process webhook calls
+async def handle(request):
+    if request.match_info.get('token') == bot.token:
+        request_body_dict = await request.json()
+        update = telebot.types.Update.de_json(request_body_dict)
+        bot.process_new_updates([update])
+        return web.Response()
     else:
-        await message.document.download(f'content{message.chat.id}.jpg')
-
-    return await message.reply("Now send a style photo")
+        return web.Response(status=403)
 
 
-@dp.message_handler(lambda message: len(message.photo) != 1, state=Form.content)
-async def process_content_invalid(message: types.Message):
-    return await message.reply("I was waiting for photo")
+app.router.add_post('/{token}/', handle)
 
 
-@dp.message_handler(lambda message: not message.photo, state=Form.style)
-async def process_style_invalid(message: types.Message):
-    return await message.reply("I was waiting for photo")
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message,
+                 ("Hi there, I am EchoBot.\n"
+                  "I am here to echo your kind words back to you."))
 
 
-@dp.message_handler(content_types=['photo', 'document'], state=Form.style)
-async def get_result(message: types.Message, state: FSMContext):
-    if message.photo:
-        await message.photo[-1].download(f'style{message.chat.id}.jpg')
-        print('downloaded')
-    else:
-        await message.document.download(f'style{message.chat.id}.jpg')
-        print('downloaded')
-
-    model = NST(128)
-    await message.answer('now wait a little')
-    print('model created')
-    res = await model.transform(f'content{message.chat.id}.jpg', f'style{message.chat.id}.jpg')
-    model.unload(res).save(f'res{message.chat.id}.jpg')
-    print('transfer done')
-    await state.finish()
-    return await message.answer_photo(types.InputFile(f'res{message.chat.id}.jpg'))
+# Handle all other messages
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+# Build ssl context
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
+
+# Start aiohttp server
+web.run_app(
+    app,
+    host=WEBHOOK_LISTEN,
+    port=WEBHOOK_PORT,
+    ssl_context=context,
+)
