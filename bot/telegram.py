8c2fb516af4b6bf05ef3fd84eebb37e4ece54a1b
jubick1337 <mattyson.so@gmail.com>
2020-06-21 19:02:09 +0500

    updated key path

--- bot/telegram.py
+++ bot/telegram.py
@@ -10,8 +10,8 @@ WEBHOOK_HOST = '35.204.74.60'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = './url_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = './url_private.key'  # Path to the ssl private key
+WEBHOOK_SSL_CERT = '../key/url_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = '../key/url_private.key'  # Path to the ssl private key
 
 WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/{}/".format(TELEGRAM_TOKEN)
