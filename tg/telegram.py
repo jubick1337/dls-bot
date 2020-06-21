a63011b73bf4dab9bf69d17c96bb6a61e60330e6
jubick1337 <mattyson.so@gmail.com>
2020-06-21 21:36:32 +0500

    downscaled

--- tg/telegram.py
+++ tg/telegram.py
@@ -70,7 +70,7 @@ def get_style(message: Message):
     with open(f'./images/style{message.chat.id}.jpg', 'wb') as file:
         file.write(downloaded_file)
 
-    model = NST(256)
+    model = NST(128)
     res = model.transform(f'./images/content{message.chat.id}.jpg', f'./images/style{message.chat.id}.jpg')
     model.unload(res).save(f'./images/res{message.chat.id}.jpg')
 
