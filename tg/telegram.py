e99eb9ab2e569f7088130e3dc27d45abf722b896
jubick1337 <mattyson.so@gmail.com>
2020-06-28 20:56:42 +0500

    added gce_wrapper

--- tg/telegram.py
+++ tg/telegram.py
@@ -59,26 +59,11 @@ def greet(message: Message):
     db_worker.set_state(message.chat.id, States.START.value)
 
 
-# try:
-#     bot.reply_to(message, 'Hi there, type one of commands: /nst /srres')
-#     db_worker.set_state(message.chat.id, States.START.value)
-# except:
-#     logger.info('smth went wrong')
-#     time.sleep(5)
-#     bot.reply_to(message, 'Hi there, type one of commands: /nst /srres')
-#     db_worker.set_state(message.chat.id, States.START.value)
-
-
 @bot.message_handler(commands=['reset'])
+@gce_wrapper
 def cmd_reset(message: Message):
-    try:
-        bot.send_message(message.chat.id, 'Hi there, type one of commands: /nst /srres')
-        db_worker.set_state(message.chat.id, States.START.value)
-    except:
-        logger.info('smth went wrong')
-        time.sleep(5)
-        bot.send_message(message.chat.id, 'Hi there, type one of commands: /nst /srres')
-        db_worker.set_state(message.chat.id, States.START.value)
+    bot.send_message(message.chat.id, 'Hi there, type one of commands: /nst /srres')
+    db_worker.set_state(message.chat.id, States.START.value)
 
 
 @bot.message_handler(commands=['help'])
@@ -91,152 +76,84 @@ def get_help(message: Message):
 
 
 @bot.message_handler(commands=['nst'])
+@gce_wrapper
 def start_nst(message: Message):
-    try:
-        bot.reply_to(message, 'Now send me content photo')
-        db_worker.set_state(message.chat.id, States.ENTER_FIRST_PIC.value)
-    except:
-        logger.info('smth went wrong')
-        time.sleep(5)
-        bot.reply_to(message, 'Now send me content photo')
-        db_worker.set_state(message.chat.id, States.ENTER_FIRST_PIC.value)
+    bot.reply_to(message, 'Now send me content photo')
+    db_worker.set_state(message.chat.id, States.ENTER_FIRST_PIC.value)
 
 
 @bot.message_handler(commands=['srres'])
+@gce_wrapper
 def start_srres(message: Message):
-    try:
-        bot.reply_to(message, 'Now send me low res photo')
-        db_worker.set_state(message.chat.id, States.ENTER_SRRES_PHOTO.value)
-    except:
-        logger.info('smth went wrong')
-        time.sleep(5)
-        bot.reply_to(message, 'Now send me low res photo')
-        db_worker.set_state(message.chat.id, States.ENTER_SRRES_PHOTO.value)
+    bot.reply_to(message, 'Now send me low res photo')
+    db_worker.set_state(message.chat.id, States.ENTER_SRRES_PHOTO.value)
 
 
 @bot.message_handler(
     func=lambda message: db_worker.get_current_state(message.chat.id) == States.ENTER_SRRES_PHOTO.value,
     content_types=['photo'])
+@gce_wrapper
 def get_low_res(message: Message):
-    try:
-        file = bot.get_file(message.photo[-1].file_id).wait()
-        downloaded_file = bot.download_file(file.file_path).wait()
+    file = bot.get_file(message.photo[-1].file_id).wait()
+    downloaded_file = bot.download_file(file.file_path).wait()
 
-        with open(f'./images/low_res{message.chat.id}.jpg', 'wb') as file:
-            file.write(downloaded_file)
+    with open(f'./images/low_res{message.chat.id}.jpg', 'wb') as file:
+        file.write(downloaded_file)
 
-        model = SRRes()
+    model = SRRes()
 
-        res = model.transform(f'./images/low_res{message.chat.id}.jpg')
-        save_image(res, f'./images/res{message.chat.id}.jpg', normalize=True)
-        # res.save(f'./images/res{message.chat.id}.jpg')
-        bot.send_photo(message.chat.id, open(f'./images/res{message.chat.id}.jpg', 'rb'))
-        db_worker.set_state(message.chat.id, States.START.value)
+    res = model.transform(f'./images/low_res{message.chat.id}.jpg')
+    save_image(res, f'./images/res{message.chat.id}.jpg', normalize=True)
+    bot.send_photo(message.chat.id, open(f'./images/res{message.chat.id}.jpg', 'rb'))
+    db_worker.set_state(message.chat.id, States.START.value)
 
-        for file in ['low_res', 'res']:
-            if os.path.exists(f'./images{file}{message.chat.id}.jpg'):
-                os.remove(f'./images{file}{message.chat.id}.jpg')
-
-    except:
-        logger.info('smth went wrong')
-        time.sleep(5)
-
-        file = bot.get_file(message.photo[-1].file_id).wait()
-        downloaded_file = bot.download_file(file.file_path).wait()
-
-        with open(f'./images/low_res{message.chat.id}.jpg', 'wb') as file:
-            file.write(downloaded_file)
-
-        model = SRRes()
-
-        res = model.transform(f'./images/low_res{message.chat.id}.jpg')
-        save_image(res, f'./images/res{message.chat.id}.jpg', normalize=True)
-        # res.save(f'./images/res{message.chat.id}.jpg')
-        bot.send_photo(message.chat.id, open(f'./images/res{message.chat.id}.jpg', 'rb'))
-        db_worker.set_state(message.chat.id, States.START.value)
-
-        for file in ['low_res', 'res']:
-            if os.path.exists(f'./images{file}{message.chat.id}.jpg'):
-                os.remove(f'./images{file}{message.chat.id}.jpg')
+    for file in ['low_res', 'res']:
+        if os.path.exists(f'./images{file}{message.chat.id}.jpg'):
+            os.remove(f'./images{file}{message.chat.id}.jpg')
 
 
 @bot.message_handler(func=lambda message: db_worker.get_current_state(message.chat.id) == States.ENTER_FIRST_PIC.value,
                      content_types=['photo'])
+@gce_wrapper
 def get_content(message: Message):
-    try:
-        file = bot.get_file(message.photo[-1].file_id).wait()
-        downloaded_file = bot.download_file(file.file_path).wait()
+    file = bot.get_file(message.photo[-1].file_id).wait()
+    downloaded_file = bot.download_file(file.file_path).wait()
 
-        with open(f'./images/content{message.chat.id}.jpg', 'wb') as file:
-            file.write(downloaded_file)
+    with open(f'./images/content{message.chat.id}.jpg', 'wb') as file:
+        file.write(downloaded_file)
 
-        bot.send_message(message.chat.id, 'Now send me style photo. If you want to specify '
-                                          'size send it within your style photo. '
-                                          'Size should be less than 512.')
-        db_worker.set_state(message.chat.id, States.ENTER_SECOND_PIC.value)
-    except:
-        logger.info('smth went wrong')
-        time.sleep(5)
-
-        file = bot.get_file(message.photo[-1].file_id).wait()
-        downloaded_file = bot.download_file(file.file_path).wait()
-
-        with open(f'./images/content{message.chat.id}.jpg', 'wb') as file:
-            file.write(downloaded_file)
-
-        bot.send_message(message.chat.id,
-                         'Now send me style photo. If you want to specify size send it within your style photo. '
-                         'Size should be less than 512.')
-        db_worker.set_state(message.chat.id, States.ENTER_SECOND_PIC.value)
+    bot.send_message(message.chat.id, 'Now send me style photo. If you want to specify '
+                                      'size send it within your style photo. '
+                                      'Size should be less than 512.')
+    db_worker.set_state(message.chat.id, States.ENTER_SECOND_PIC.value)
 
 
 @bot.message_handler(func=lambda message: db_worker.get_current_state(message.chat.id) == States.ENTER_SECOND_PIC.value,
                      content_types=['photo'])
+@gce_wrapper
 def get_style(message: Message):
-    try:
-        file = bot.get_file(message.photo[-1].file_id).wait()
-        downloaded_file = bot.download_file(file.file_path).wait()
+    file = bot.get_file(message.photo[-1].file_id).wait()
+    downloaded_file = bot.download_file(file.file_path).wait()
 
-        with open(f'./images/style{message.chat.id}.jpg', 'wb') as file:
-            file.write(downloaded_file)
+    with open(f'./images/style{message.chat.id}.jpg', 'wb') as file:
+        file.write(downloaded_file)
 
-        if message.caption:
-            if 1 <= int(message.caption) <= 512:
-                model = NST(int(message.caption))
-            else:
-                model = NST(256)
+    if message.caption:
+        if 1 <= int(message.caption) <= 512:
+            model = NST(int(message.caption))
         else:
             model = NST(256)
-        res = model.transform(f'./images/content{message.chat.id}.jpg', f'./images/style{message.chat.id}.jpg')
-        save_image(res, f'./images/res{message.chat.id}.jpg', normalize=True)
-        # model.unload(res).save(f'./images/res{message.chat.id}.jpg')
+    else:
+        model = NST(256)
+    res = model.transform(f'./images/content{message.chat.id}.jpg', f'./images/style{message.chat.id}.jpg')
+    save_image(res, f'./images/res{message.chat.id}.jpg', normalize=True)
 
-        bot.send_photo(message.chat.id, open(f'./images/res{message.chat.id}.jpg', 'rb'))
-        db_worker.set_state(message.chat.id, States.START.value)
+    bot.send_photo(message.chat.id, open(f'./images/res{message.chat.id}.jpg', 'rb'))
+    db_worker.set_state(message.chat.id, States.START.value)
 
-        for file in ['content', 'style', 'res']:
-            if os.path.exists(f'./images{file}{message.chat.id}.jpg'):
-                os.remove(f'./images{file}{message.chat.id}.jpg')
-
-    except:
-        logger.info('smth went wrong')
-        time.sleep(5)
-        file = bot.get_file(message.photo[-1].file_id).wait()
-        downloaded_file = bot.download_file(file.file_path).wait()
-
-        with open(f'./images/style{message.chat.id}.jpg', 'wb') as file:
-            file.write(downloaded_file)
-
-        model = NST(128)
-        res = model.transform(f'./images/content{message.chat.id}.jpg', f'./images/style{message.chat.id}.jpg')
-        model.unload(res).save(f'./images/res{message.chat.id}.jpg')
-
-        bot.send_photo(message.chat.id, open(f'./images/res{message.chat.id}.jpg', 'rb'))
-        db_worker.set_state(message.chat.id, States.START.value)
-
-        for file in ['content', 'style', 'res']:
-            if os.path.exists(f'./images{file}{message.chat.id}.jpg'):
-                os.remove(f'./images{file}{message.chat.id}.jpg')
+    for file in ['content', 'style', 'res']:
+        if os.path.exists(f'./images{file}{message.chat.id}.jpg'):
+            os.remove(f'./images{file}{message.chat.id}.jpg')
 
 
 bot.remove_webhook()
